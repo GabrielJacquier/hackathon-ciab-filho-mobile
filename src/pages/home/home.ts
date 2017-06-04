366a58697e7465e29e614410ce7d61e112a67a9b
Samuel <samuel.pisa@tegra.me>
2017-06-04 01:35:13 -0300

    adiciona extrato

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,28 +13,73 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home.html',
 })
 export class HomePage {
-
-  items = [
-    'Pokémon Yellow',
-    'Super Metroid',
-    'Mega Man X',
-    'The Legend of Zelda',
-    'Pac-Man',
-    'Super Mario World',
-    'Street Fighter II',
-    'Half Life',
-    'Final Fantasy VII',
-    'Star Fox',
-    'Tetris',
-    'Donkey Kong III',
-    'GoldenEye 007',
-    'Doom',
-    'Fallout',
-    'GTA',
-    'Halo'
-  ];
+  private item:any;
+  private extratos:any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.item="extrato";
+    this.extratos = [
+      {
+        'data':'Hoje',
+        'descricao':'Burger King',
+        'categoria':'Alimentação',
+        'valor':24.50
+      },
+      {
+        'data':'Ontem',
+        'descricao':'Apple Store',
+        'categoria':'Games',
+        'valor':3.70
+      },
+      {
+        'data':'01/05/2017',
+        'descricao':'Cantina do Pepe',
+        'categoria':'Alimentação',
+        'valor':13.22
+      },
+      {
+        'data':'01/05/2017',
+        'descricao':'Netflix',
+        'categoria':'Entretenimento',
+        'valor':16.90
+      },
+      {
+        'data':'28/04/2017',
+        'descricao':'Padaria Real',
+        'categoria':'Varejo',
+        'valor':4.70
+      },
+      {
+        'data':'27/04/2017',
+        'descricao':'Uber BR',
+        'categoria':'Transporte',
+        'valor':12.70
+      },
+      {
+        'data':'01/05/2017',
+        'descricao':'Cantina do Pepe',
+        'categoria':'Alimentação',
+        'valor':13.22
+      },
+      {
+        'data':'01/05/2017',
+        'descricao':'Papelaria Silvana',
+        'categoria':'Varejo',
+        'valor':9.22
+      },
+      {
+        'data':'23/05/2017',
+        'descricao':'Padaria Real',
+        'categoria':'Varejo',
+        'valor':4.70
+      },
+      {
+        'data':'22/04/2017',
+        'descricao':'SmartFit',
+        'categoria':'Saúde',
+        'valor':59.90
+      }
+    ];
   }
 
   ionViewDidLoad() {
